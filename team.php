e05b0b45baf645ef3f52e4073cade683b6485e24
Damir Destani <destani.d98@gmail.com>
2023-02-11 19:56:25 +0100

    team-page

---
+++ team.php
@@ -0,0 +1,81 @@
+<?php
+
+include('config/db_connect.php');
+
+if (isset($_GET['id'])) {
+    $teamid = mysqli_real_escape_string($conn, $_GET['id']);
+}
+
+$query = "SELECT * FROM team WHERE id = $teamid";
+$result = mysqli_query($conn, $query);
+$team = mysqli_fetch_assoc($result);
+mysqli_free_result($result);
+
+$query = "SELECT * FROM player WHERE teamid='$teamid'";
+$result = mysqli_query($conn, $query);
+$players = mysqli_fetch_all($result, MYSQLI_ASSOC);
+mysqli_free_result($result);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include('templates/header.php'); ?>
+
+<?php if ($team == null) : ?>
+
+    <h1>There is no team with that ID!</h1>
+    <div class="center">
+        <a href="index.php" class="btn center brown darken-2">Return</a>
+    </div>
+
+<?php else : ?>
+
+    <div class="container center">
+        <div class="card z-depth-0 radius-card" style="padding-bottom: 30px;">
+            <img src="<?php echo $team['img']; ?>" alt="icon" class="icon-card">
+            <h3 class="center"><?php echo $team['name']; ?></h3>
+            <h5 class="center">Founded in <?php echo $team['founded']; ?></h5>
+            <h5 class="center">Location: <?php echo $team['city']; ?></h5>
+            <h5 class="center">Arena: <?php echo $team['stadium']; ?></h5>
+            <h5 class="center">Number of rings: <?php echo $team['titles']; ?></h5>
+            <h5 class="center">Manager: <?php echo $team['manager']; ?></h5>
+        </div>
+    </div>
+
+    <?php if ($players == null) { ?>
+
+        <h6 class="center">No players in database! </h6>
+
+    <?php } else {; ?>
+
+        <div class="container">
+            <h5 class="center">Players in database:</h5>
+            <div class="row">
+                <?php foreach ($players as $player) : ?>
+                    <div class="col s12 m6 l4 xl3">
+                        <div class="card z-depth-0 radius-card">
+                            <img src="<?php echo $team['img']; ?>" alt="icon" class="icon-card">
+                            <div class="card-content center">
+                                <h5><?php echo htmlspecialchars($player['name']); ?></h5>
+                            </div>
+                            <div class="card-action right-align radius-card">
+                                <a href="player.php?id=<?php echo $player['id']; ?>" class="brown-text text-darken-2">
+                                    More Info
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+
+    <?php }; ?>
+
+<?php endif; ?>
+
+
+<?php include('templates/footer.php'); ?>
+
+</html>
